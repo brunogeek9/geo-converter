1ae5b5a17e5602e962f827204e40b0e74b40e992
brunogeek9 <brunogeek9@gmail.com>
2019-10-29 15:15:33 -0300

    removendo arquivos sem uso

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var Promise = require('bluebird');
+var GeoJSON = require('mongoose-geojson-schema');
 const mongo = Promise.promisifyAll(require('mongoose'));
 
 var mydata;
 mongo.connect(
     'mongodb://localhost:27017/geoconv',
     { useNewUrlParser: true }
- 
+
 );
 var db = mongo.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -13,51 +14,50 @@ db.once('open', function () {
     console.log('conectado');
 });
 async function consoleAsync(data) {
-    // mydata = data;
-    // await console.log(data);
-} 
+    await console.log(data);
+}
 var restaurantSchema = new mongo.Schema({
     address: {
-        // building: mongo.Types.Decimal128,
-        // coord: [[ {lat: Number}, {long: Number }]],
         street: String,
-        // zipcode: mongo.Types.Decimal128
- 
     },
     borough: String,
     cuisine: String,
     grades: [{
         grade: String,
-        // score: mongo.Types.Decimal128,
         date: Date
     }],
     name: String,
     restaurant_id: Number,
-    coordinates: {type: "Point}
- 
+    point: mongo.Schema.Types.Point,
 });
 
 var Model = mongo.model('restaurants', restaurantSchema);
-var finalResults = [];
-// var query = Model.findOne({}
-    // , function (err, data) {
-    // if (err) return console.error(err);
-    // // consoleAsync(data);
-// );
-var promises = Model.find({}, function(err, results) {
-    results.forEach(function(element) {
-        finalResults.push(element);
-    });
-});
+// var finalResults = [];
 
-Promise.all(promises).then(function() {
-    console.log(finalResults.length);
-}).error(console.error);
+// var promises = Model.find({}, function (err, results) {
+//     results.forEach(function (element) {
+//         finalResults.push(element);
+//     });
+// });
 
-console.log(finalResults[0]);
+// Promise.all(promises).then(function () {
+//     // Model.updateMany({}, { $set: { point: finalResults.address.coord } });
+//     var tam = finalResults.length;
+//     console.log(finalResults[0]);
+//     for (let index = 0; index < tam; index++) {
+//         Model.update({}, { $set: { point: finalResults[index].address.coord } });
+//     }
+//     console.log(finalResults[0].address.building);
+// }).error(console.error);
+
+Model.findOne({}, function (err, data) {
+    consoleAsync(data.address);
+})
+
+// console.log(finalResults[0]);
 // var promise = query.exec();
 // promise.addBack(function (err, docs) {
 //     mydata = docs;
 // });
 // console.log(mydata);
-// await Model.updateMany({}, { $set: { name: 'foo' } });
+// await Model.updateMany({}, { $set: { point: [] } });
