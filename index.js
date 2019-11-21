e53d19257c9429df1ebc736d779900a69727126e
brunogeek9 <brunogeek9@gmail.com>
2019-11-21 13:35:04 -0300

    switch case para opções de execução implementado

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Promise = require('bluebird');
+const OP = 2;
 // var GeoJSON = require('mongoose-geojson-schema');
 const mongo = Promise.promisifyAll(require('mongoose'));
 const PORT = 27017;
@@ -31,48 +32,61 @@ var promises = Model.find({}, function (err, results) {
     });
 });
 
-//percorrendo o array da collection para converter os campos para padrão GeoJSON
-console.time(labels.get('t1'));
-Promise.all(promises).then(function () {
-    var tam = finalResults.length;
+switch (OP) {
+    case 1:
+        //percorrendo o array da collection para converter os campos para padrão GeoJSON
+        console.time(labels.get('t1'));
+        Promise.all(promises).then(function () {
+            var tam = finalResults.length;
 
-    for (let index = 0; index < tam; index++) {
-        var local = finalResults[index].address.coord;
-        finalResults[index].updateOne(
+            for (let index = 0; index < tam; index++) {
+                var local = finalResults[index].address.coord;
+                finalResults[index].updateOne(
+                    {
+                        location: local
+                    },
+                    { multi: true },
+                    function (err, numberAffected) {
+                    });
+            }
+            console.timeEnd(labels.get('t1'));
+            // console.log(local);
+        }).error(console.error);
+
+
+        break;
+    case 2:
+        //buscando os restaurantes distantes até 1 quilômetro do Port Authority Bus Terminal (NY)
+        console.time(labels.get('t2'))
+        Model.find({
+            location:
             {
-                location: local
-            },
-            { multi: true },
-            function (err, numberAffected) {
-            });
-    }
-    // console.log(local);
-}).error(console.error);
-console.time(labels.get('t1'));
+                $near:
+                {
+                    $geometry:
+                        { type: "Point", coordinates: [-73.9903, 40.7570] },
+                    $minDistance: 0, $maxDistance: 1000
+                }
+            }
+        }, function (error, d) {
+            // console.log(d.length)
+            if (error) return console.error(error);
+            console.timeEnd(labels.get('t2'));
 
-//buscando os restaurantes distantes até 1 quilômetro do Port Authority Bus Terminal (NY)
-console.time(labels.get('t2'))
-Model.find({
-    location:
-    {
-        $near:
-        {
-            $geometry:
-                { type: "Point", coordinates: [-73.9903, 40.7570] },
-            $minDistance: 0, $maxDistance: 1000
-        }
-    }
-}, function (err, d) {
-    // console.log(d.length)
-    if (err) return console.error(err);
-    console.time(labels.get('t2'));
+        });
+        break;
+    case 3:
+        //buscando todos os restaurantes
+        console.time(labels.get('t3'));
+        Model.find({}, function (error, data) {
+            // console.log(d.length)
+            if (error) return console.error(error);
+            console.timeEnd(labels.get('t3'));
+        })
 
-});
+        break;
+    default:
+        break;
+}
 
-//buscando todos os restaurantes
-console.time(labels.get('t3'));
-Model.find({},function(error,data){
-    // console.log(d.length)
-    if (err) return console.error(err);
-    console.time(labels.get('t3')); 
-})
+
